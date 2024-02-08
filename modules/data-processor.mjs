ffe1604bce7bd4035244bd19c594539ddeb0432b
Marie CHANARON <marie.chanaron@capgemini.com>
2024-02-08 12:21:31 +0100

    Test: get issue keys

--- modules/data-processor.mjs
+++ modules/data-processor.mjs
@@ -1,22 +1,48 @@
+const twoDArray = [];
+
 const processData = content => {
 
     const rowsArray = content.split(/\r\n/);
-    const twoDArray = [];
-
+    
     rowsArray.forEach(line => {
         const columnsArray = line.split(';');
         twoDArray.push(columnsArray);
     });
 
-    return convertData(twoDArray);    
+    console.log(rowsArray);
+    console.log(twoDArray);
+
+    return convertData();    
 }
 
-const convertData = initialData => {
+const convertData = () => {
 
-    const convertedData = initialData; 
+    // const convertedData = twoDArray;
+    const convertedData = []; 
     // TODO: convertir/transformer les données
-   
+
+    // Issue Key
+    // const columnName = "Status";
+    // const header = initialData[0];
+    // const index = header.indexOf(columnName);
+    // console.log("Index du status :" + index);
+    const index = getColumnIndex("Issue key");
+    console.log(`index: ${index}`);
+
+    // const colIndexInNewTable = 0;
+    for (let i = 0; i < twoDArray.length; i ++) {
+        const value = twoDArray[i][index];
+        console.log(value);
+        
+    }
+    
+
     return convertedData;
+}
+
+const getColumnIndex = columnName => {
+    const header = twoDArray[0];
+    return header.indexOf(columnName);
 }
 
 const parseJson = jsonString => {
