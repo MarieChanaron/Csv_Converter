8585fffe5910d0ae3853a27c4cdbd0ecfe32e97c
Marie CHANARON <marie.chanaron@capgemini.com>
2024-02-08 11:13:41 +0100

    Parse the Json string and return 3 columns (action, data, result)

--- modules/data-processor.mjs
+++ modules/data-processor.mjs
@@ -15,8 +15,17 @@ const convertData = initialData => {
 
     const convertedData = initialData; 
     // TODO: convertir/transformer les données
-
+   
     return convertedData;
+}
+
+const parseJson = jsonString => {
+    const jsonObject = JSON.parse(jsonString);
+    const fields = jsonObject[0].fields;
+    const action = fields['Action'];
+    const data = fields['Data'];
+    const result = fields['Expected Result'];
+    return [[action], [data], [result]];
 }
 
 export default processData;
