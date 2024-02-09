dc75dd9d7aa33d23bc1f646ec52853c0fbc0514f
Marie CHANARON <marie.chanaron@capgemini.com>
2024-02-09 15:29:22 +0100

    Program working okay except: semicolons - dates formatting

--- modules/data-processor.mjs
+++ modules/data-processor.mjs
@@ -5,8 +5,8 @@ let columnsCount = 0;
 
 /*
 Correspondance between the input file and the output file.
-On the left: input file
-On the right: output file
+On the left (key): input file
+On the right (value): output file
 */
 const headers = {
     "Issue Key": "Issue key",
@@ -24,6 +24,8 @@ const headers = {
     "Test Repository Path": "Custom field (Test Repository Path)",
     "Link \"Tests\"": "Outward issue link (Tests)",
     "Link \"Defect\"": "Outward issue link (Defect)",
+    "Link \"Blocks\"": "Outward issue link (Blocks)",
+    "Link \"Relates\"": "Outward issue link (Relates)",
     "Link \"Cloners\"": "Outward issue link (Cloners)",
     "Reporter": "Reporter",
     "Assignee": "Assignee",
@@ -82,10 +84,6 @@ const copyPasteValues = (inputHeader, outputHeader) => {
 
         for (let indexLine = 1; indexLine < twoDArray.length; indexLine ++) {
             const value = twoDArray[indexLine][indexCol];
-            // if (outputHeader === 'Description' && value) {
-            //     console.log(indexLine);
-            //     console.log(value.split('€'));
-            // }
             convertedData[indexLine][length] = value ? value : ''; // Add an empty string if there is no value (to avoid bugs later on when adding test steps)
         }
         indexCol ++;
@@ -102,7 +100,8 @@ const formatJsonString = jsonString => {
 }
 
 
-const handleLineBreaks = string => {
+// Do not parse \n, \r and semicolons inside of a single cell
+const doNotParse = string => {
     string = string.replace(/"/g, '""');
     return `"${string}"`;
 }
@@ -117,11 +116,21 @@ const getIssueLineInNewTable = issueKey => {
 }
 
 
+// Insert new lines inside of an array
+// linesArray: Array containing all of the lines to add in the table
+// Position: index of the new line
+const insertNewLines = (linesArray, position) => {
+    const firstPart = convertedData.slice(0, position);
+    const secondPart = convertedData.slice(position);
+    convertedData = firstPart.concat(linesArray).concat(secondPart);
+}
+
+
 const addManualTestSteps = () => {
     const headerInEntryFile = "Custom field (Manual Test Steps)";
     const colIndex = getColumnIndex(headerInEntryFile);
 
-    // Récupération de la position des colonnes Action, Data et Result dans le nouveau tableau
+    // Retrieval of the column positions of Action, Data and Result columns (in the new table)
     const actionIndex = getColumnIndex("Action", convertedData);
     const dataIndex = getColumnIndex("Data", convertedData);
     const resultIndex = getColumnIndex("Result", convertedData);
@@ -134,7 +143,7 @@ const addManualTestSteps = () => {
         let issueIndex = getIssueLineInNewTable(issueKey);
 
         // Add the TCID
-        if (issueKey) {
+        if (issueKey && convertedData[issueIndex]) {
             convertedData[issueIndex][tcidIndex] = i.toString();
         }
 
@@ -152,31 +161,26 @@ const addManualTestSteps = () => {
 
         testStepsJsonObject.forEach((testStep, pos) => {
             const fields = testStep.fields;
-            const action = handleLineBreaks(fields['Action']);
-            console.log(action);
-            const data = handleLineBreaks(fields['Data']);
-            const result = handleLineBreaks(fields['Expected Result']);
+            const action = doNotParse(fields['Action']);
+            const data = doNotParse(fields['Data']);
+            const result = doNotParse(fields['Expected Result']);
             if (pos === 0) { // Add the first test step directly in the issue line
                 convertedData[issueIndex][actionIndex] = action;
                 convertedData[issueIndex][dataIndex] = data;
                 convertedData[issueIndex][resultIndex] = result;
-            } 
-            else { // For the additional test steps: make new lines
+            } else { // For the additional test steps: make new lines
                 const newLine = new Array(convertedData[0].length);
                 newLine.fill('');
                 newLine[actionIndex] = action;
                 newLine[dataIndex] = data;
                 newLine[resultIndex] = result;
-                // Add the TCID in the new line
-                newLine[tcidIndex] = i.toString();
+                newLine[tcidIndex] = i.toString(); // Add the TCID in the new line
                 newLines.push(newLine);
             }
         });
 
         // Add the new lines to the convertedData array
-        const firstPart = convertedData.slice(0, issueIndex + 1);
-        const secondPart = convertedData.slice(issueIndex + 1);
-        convertedData = firstPart.concat(newLines).concat(secondPart);
+        insertNewLines(newLines, issueIndex + 1);
     }
 }
 
