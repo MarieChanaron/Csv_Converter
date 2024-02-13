6569b3e42bb482d4a8d62f062a233c989830c87d
Marie CHANARON <marie.chanaron@capgemini.com>
2024-02-13 16:55:45 +0100

    Restructure the code in app.js

--- app.js
+++ app.js
@@ -2,10 +2,46 @@ import readFile from "./modules/csv-reader.mjs";
 import processData from "./modules/data-processor.mjs";
 import writeCsv from "./modules/csv-maker.mjs";
 
+
+const showSpinner= () => {
+    const spinner = document.getElementById('spinner');
+    spinner.classList.remove('display-none');
+}
+
+
+const hideSpinner = () => {
+    const spinner = document.getElementById('spinner');
+    spinner.classList.add('display-none');
+}
+
+
+const hideLogs = () => {
+    const logsDiv = document.getElementById('logs');
+    logsDiv.setAttribute('hidden', 'true');
+
+    // Remove all the DOM elements containing errors (otherwise some errors can be added to the previous errors)
+    const errorsDiv = document.getElementById('errors');
+    while (errorsDiv.firstChild) {
+        errorsDiv.removeChild(errorsDiv.firstChild);
+    }
+}
+
+
+const showLogs = (data) => {
+    const logsDiv = document.getElementById('logs');
+    logsDiv.removeAttribute('hidden');
+    if (data.length > 0) {
+        confirmation.innerHTML = 'Le fichier a bien été généré. <img src="/images/checked.png" alt="icon" class="icon">';
+    } else {
+        confirmation.innerHTML = "Le fichier n'a pas pu être généré.";
+    }
+}
+
+
 window.onload = () => {
     document.querySelector('form').onsubmit = handleSubmit;
-    document.querySelector('input[type="submit"]').onclick = handleClick;
 }
+
 
 const handleSubmit = async(event) => {
     event.preventDefault();
@@ -13,25 +49,15 @@ const handleSubmit = async(event) => {
     if (!file) {
         alert('Merci de choisir un fichier.');
     } else {
-        const spinner = document.getElementById('spinner');
-        spinner.classList.remove('display-none');
+        console.clear();
+        showSpinner();
+        hideLogs();
+
         const fileContent = await readFile(file);
         const twoDArray = processData(fileContent);
-        spinner.style.display = 'none';
         writeCsv(twoDArray);
-        const confirmationDiv = document.getElementById('confirmation');
-        if (twoDArray.length > 1) confirmationDiv.removeAttribute('hidden');
-        const logsDiv = document.getElementById('logs');
-        logsDiv.removeAttribute('hidden');
-    }
-}
 
-const handleClick = () => {
-    console.clear();
-    const errorsDiv = document.getElementById('errors');
-    while (errorsDiv.firstChild) {
-        errorsDiv.removeChild(errorsDiv.firstChild);
+        hideSpinner();
+        showLogs(twoDArray);
     }
-    const confirmationDiv = document.getElementById('confirmation');
-    confirmationDiv.setAttribute('hidden', 'false');
 }
