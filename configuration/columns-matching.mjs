ee1252c153cad0453ae34aec05fe199db66c4a08
Marie CHANARON <marie.chanaron@capgemini.com>
2024-02-14 14:03:12 +0100

    Move the headers object into a separate file

---
+++ configuration/columns-matching.mjs
@@ -0,0 +1,32 @@
+/*
+Matching between the input file and the output file.
+On the left (key): input file
+On the right (value): output file
+*/
+export const headers = {
+    "Issue Key": "Issue key",
+    "Type": "Issue Type",
+    "Test Type": "Custom field (Test Type)",
+    "Status": "Status",
+    "TCID": undefined,
+    "Summary": "Summary",
+    "Description": "Description",
+    "Action": undefined,
+    "Data": undefined,
+    "Result": undefined,
+    "Priority": "Priority",
+    "Components": "Component/s",
+    "Test Repository Path": "Custom field (Test Repository Path)",
+    "Link \"Tests\"": "Outward issue link (Tests)",
+    "Link \"Defect\"": "Outward issue link (Defect)",
+    "Link \"Blocks\"": "Outward issue link (Blocks)",
+    "Link \"Relates\"": "Outward issue link (Relates)",
+    "Link \"Cloners\"": "Outward issue link (Cloners)",
+    "Reporter": "Reporter",
+    "Assignee": "Assignee",
+    "Label": "Labels",
+    "Custom field (Test Level)": "Custom field (Test Level)",
+    "Custom field (Steps Count)": "Custom field (Steps Count)",
+    "Environment": "Environment",
+    "Created": "Created"
+}
--- modules/data-processor.mjs
+++ modules/data-processor.mjs
@@ -1,40 +1,9 @@
+import { headers } from "../data/columns-matching.mjs";
+
+
 let initialData = [];
 let convertedData = [];
 let columnsCount = {};
-
-
-/*
-Matching between the input file and the output file.
-On the left (key): input file
-On the right (value): output file
-*/
-const headers = {
-    "Issue Key": "Issue key",
-    "Type": "Issue Type",
-    "Test Type": "Custom field (Test Type)",
-    "Status": "Status",
-    "TCID": undefined,
-    "Summary": "Summary",
-    "Description": "Description",
-    "Action": undefined,
-    "Data": undefined,
-    "Result": undefined,
-    "Priority": "Priority",
-    "Components": "Component/s",
-    "Test Repository Path": "Custom field (Test Repository Path)",
-    "Link \"Tests\"": "Outward issue link (Tests)",
-    "Link \"Defect\"": "Outward issue link (Defect)",
-    "Link \"Blocks\"": "Outward issue link (Blocks)",
-    "Link \"Relates\"": "Outward issue link (Relates)",
-    "Link \"Cloners\"": "Outward issue link (Cloners)",
-    "Reporter": "Reporter",
-    "Assignee": "Assignee",
-    "Label": "Labels",
-    "Custom field (Test Level)": "Custom field (Test Level)",
-    "Custom field (Steps Count)": "Custom field (Steps Count)",
-    "Environment": "Environment",
-    "Created": "Created"
-}
 
 
 // This function returns an object where:
